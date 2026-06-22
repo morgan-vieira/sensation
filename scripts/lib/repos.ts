ba6f7927e31a9b623095dbc8eb18984c24ffeabd
morgan-vieira <266319256+morgan-vieira@users.noreply.github.com>
2026-06-23 00:04:40 +0100

    style(scripts): apply Prettier formatting

--- scripts/lib/repos.ts
+++ scripts/lib/repos.ts
@@ -1,20 +1,20 @@
 export interface RepoVersion {
-  sourcePath: string;
-  valuePath: string[];
-  tagPrefix: string;
+	sourcePath: string
+	valuePath: string[]
+	tagPrefix: string
 }
 
 export interface RepoConfig {
-  id: string;
-  url: string;
-  ref?: string;
-  version?: RepoVersion;
+	id: string
+	url: string
+	ref?: string
+	version?: RepoVersion
 }
 
 export const repos: ReadonlyArray<RepoConfig> = [
-  {
-    id: "LibStudioElttob",
-    url: "https://github.com/Elttob/LibStudioElttob.git",
-    ref: "main",
-  },
-];
+	{
+		id: "LibStudioElttob",
+		url: "https://github.com/Elttob/LibStudioElttob.git",
+		ref: "main",
+	},
+]
